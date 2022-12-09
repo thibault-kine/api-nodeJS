077395c17a6b6cff220ba1ec5a138336efedfce1
thibault-kine <thibault.kine@laplateforme.io>
2022-12-09 15:41:40 +0100

    Le token marche

--- app/Controllers/UserController.js
+++ app/Controllers/UserController.js
@@ -10,21 +10,25 @@ class User {
      */
     static login(req, res) {
         if(!req.body.email || !req.body.password) return res.status(400).send('Email ou mot de passe manquant');
-        let user = {};
 
-        const sql = 'SELECT * FROM users';
+        const sql = `SELECT * FROM users WHERE email = '${req.body.email}' AND password = '${req.body.password}'`;
+
         db.query(sql, (error, result) => {
             if(error) {
                 res.send(error);
             }
-            user = result;
+            
+            const token = generateAccessToken({
+                id: result[0].id,
+                email: result[0].email,
+                password: result[0].password,
+                roles: result[0].roles,
+                firstname: result[0].firstname,
+                lastname: result[0].lastname
+            });
+
+            return res.json({ access_token: token });
         })
-
-        if(!user) return res.status(400).send('Utilisateur introuvable');
-
-        const token = generateAccessToken({ id: user.id, roles: user.roles });
-
-        return res.json({ access_token: token });
     }
 
     /**
@@ -82,7 +86,7 @@ class User {
      */
     static getMe(req, res) {
         const token = req.headers.authorization && extractBearerToken(req.headers.authorization);
-        const decoded = jwt.decode(token, { complete: false });
+        const decoded = jwt.decode(token);
 
         return res.json({ content: decoded });
     }
@@ -92,7 +96,31 @@ class User {
      * Modifier ses informations
      */
     static putMe(req, res) {
-        res.send('Modifier ses informations persos');
+        const token = req.headers.authorization && extractBearerToken(req.headers.authorization);
+        const decoded = jwt.decode(token);
+
+        let { email, roles, password, firstname, lastname } = req.body;
+        if(!req.body.email || !req.body.roles || !req.body.password || !req.body.firstname || !req.body.lastname) {
+            return res.status(400).send('Champs manquants');
+        }
+
+        const sql = `UPDATE users SET email = '${email}', roles = '${roles}', password = '${password}', firstname = '${firstname}', lastname = '${lastname}' WHERE id = '${decoded.id}'`;
+        db.query(sql, (error, result) => {
+            if(error) {
+                res.send(error);
+            }
+
+            const token = generateAccessToken({
+                id: decoded.id,
+                email: email,
+                password: password,
+                roles: roles,
+                firstname: firstname,
+                lastname: lastname
+            });
+
+            return res.json({ access_token: token });
+        })
     }
 
     /**
